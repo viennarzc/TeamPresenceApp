6acd97cb0240ccc62f7d292c254fa7f2bc89e89a
Viennarz Curtiz <viennarzc@gmail.com>
2024-10-09 06:24:12 +0800

    Create Project

---
+++ TeamPresence/TeamPresenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamPresenceApp.swift
+//  TeamPresence
+//
+//  Created by Viennarz Curtiz on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamPresenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeamPresenceTests/TeamPresenceTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeamPresenceTests.swift
+//  TeamPresenceTests
+//
+//  Created by Viennarz Curtiz on 10/9/24.
+//
+
+import Testing
+@testable import TeamPresence
+
+struct TeamPresenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

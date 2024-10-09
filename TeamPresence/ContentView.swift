4651178ffedf4d8f17045c7f50dee4d2510738b0
Viennarz Curtiz <viennarzc@gmail.com>
2024-10-09 21:44:33 +0800

    Add TabView

--- TeamPresence/ContentView.swift
+++ TeamPresence/ContentView.swift
@@ -9,25 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     private var columns: [GridItem] = [
-        .init(.adaptive(minimum: 120, maximum: 300), spacing: 8),
-        .init(.adaptive(minimum: 120, maximum: 300), spacing: 8),
+        .init(
+            .adaptive(minimum: 120, maximum: 300),
+            spacing: 8,
+            alignment: .center
+        ),
+        .init(
+            .adaptive(minimum: 120, maximum: 300),
+            spacing: 8,
+            alignment: .center
+        ),
     ]
 
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 16) {
-                ForEach(0 ..< 10) { _ in
-
-                    MemberCardView()
-                        .frame(height: 280)
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
-                        .background(content: {
-                            RoundedRectangle(cornerRadius: 12)
-                                .stroke(Color.gray, lineWidth: 0.5)
-                        })
+        TabView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(0 ..< 10) { _ in
+                        MemberCardView()
+                            .frame(height: 280)
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .background(content: {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(Color.gray, lineWidth: 0.5)
+                            })
+                    }
                 }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
+            .tabItem {
+                Label("Space", systemImage: "person.3")
+            }
         }
         .preferredColorScheme(.light)
     }
